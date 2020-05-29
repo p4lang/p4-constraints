be65e27b82a7b3d9e51ffdce47211960fd34032a
Steffen Smolka <smolkaj@google.com>
2020-05-29 15:59:37 -0700

    Also format Bazel files using buildifier.

--- format.sh
+++ format.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# Formats source files according to Google's style guide. Requires clang-tidy.
+# Formats source files according to Google's style guide. Requires clang-format.
 
-# Only files with these extensions will be formatted.
-EXTENSIONS="cc|h|proto"
+# Only files with these extensions will be formatted by clang-format.
+CLANG_FORMAT_EXTENSIONS="cc|h|proto"
 
+# Run clang-format.
 find . -not -path ".third_party/**" \
-  | egrep "\.(${EXTENSIONS})\$" \
+  | egrep "\.(${CLANG_FORMAT_EXTENSIONS})\$" \
   | xargs clang-format -style=google -i
+
+# Run buildifier (Bazel file formatter).
+bazel run //:buildifier
