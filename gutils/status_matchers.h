5f23765be161c6a3d8d6108e3b63b084b8df99a2
Andy Fingerhut <andy.fingerhut@gmail.com>
2025-02-05 01:26:54 -0500

    Add SPDX license identifier comment to most source files
    Signed-off-by: Andy Fingerhut <andy.fingerhut@gmail.com>

--- gutils/status_matchers.h
+++ gutils/status_matchers.h
@@ -1,6 +1,7 @@
 // Copied and adapted from https://github.com/google/iree.
 
 // Copyright 2019 Google LLC
+// SPDX-License-Identifier: Apache-2.0
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
